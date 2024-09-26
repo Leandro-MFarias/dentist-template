90acfc9216d9974787225873d67c05421fc8a3c2
Leandro Farias <leandrorf1606@gmail.com>
2024-09-26 16:16:20 -0300

    agora vai

--- src/components/ButtonZap/index.jsx
+++ src/components/ButtonZap/index.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export function ButtonZap() {
     return (
-        <button className="fixed top-[92%] right-[1%] bg-green-400 hover:bg-green-500 p-4 rounded-full">
+        <button className="fixed top-[88%] sm:top-[90%] right-[1%] bg-green-400 hover:bg-green-500 p-4 rounded-full">
             <a href="#" className="text-white text-4xl">
                 <FaWhatsapp />
             </a>
